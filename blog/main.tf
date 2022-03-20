069f5f005b1f617215e77959c96514c75b4687d1
Jack Dockerty <jdockerty19@gmail.com>
2022-03-20 23:02:11 +0000

    add blog terraform code

---
+++ blog/main.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_app" "blog" {
+  spec {
+    name   = "blog"
+    region = "lon"
+
+    # Still need to use a CNAME record from the given DO app link :(
+    domain {
+        name = "blog.jdocklabs.co.uk"
+        type = "PRIMARY"
+    }
+
+    static_site {
+      name          = "personal-blog"
+      source_dir    = "blog/"
+      build_command = "rm -r ./public; hugo --destination ./public"
+      output_dir    = "public"
+
+      github {
+        repo   = "jdockerty/blog"
+        branch = "main"
+      }
+    }
+  }
+}
